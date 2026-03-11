ebe084b36da99b5144674c35d4120997d1588d60
Adele-dll <adele.dalle@orange.fr>
2026-03-10 21:29:01 -0400

    cube

--- script/script1.js
+++ script/script1.js
@@ -317,7 +317,7 @@ function initCube() {
     const geometry=new THREE.BoxGeometry(4,4,4);
     const cube=new THREE.Mesh(geometry,materials);
 
-    cube.rotation.x=0.5;
+    cube.rotation.x=0.9;
     cube.rotation.y=0.8;
 
     scene.add(cube);
