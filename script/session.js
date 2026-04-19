0654e55c32df3db8512815d133914e4a46fa56d9
Adele-dll <adele.dalle@orange.fr>
2026-04-19 14:59:29 -0400

    cube

--- script/session.js
+++ script/session.js
@@ -479,14 +479,14 @@ function applyMode(scene, cube) {
         backFaceMesh = new THREE.Mesh(plane,
             new THREE.MeshStandardMaterial({ map: createKeyboardFace(faceBack), side: THREE.DoubleSide })
         );
-        backFaceMesh.position.set(7, -1, 0);
-        backFaceMesh.rotation.z = -0.03;
+        backFaceMesh.position.set(7, -3.5, 0);
+        backFaceMesh.rotation.z = -0.12;
         scene.add(backFaceMesh);
 
         rightFaceMesh = new THREE.Mesh(plane,
             new THREE.MeshStandardMaterial({ map: createKeyboardFace(faceRight), side: THREE.DoubleSide })
         );
-        rightFaceMesh.position.set(-7.5, 1, 0);
+        rightFaceMesh.position.set(-7.5, 3.5, 0);
         scene.add(rightFaceMesh);
     }
 
